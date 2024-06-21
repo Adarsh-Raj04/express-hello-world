5e518a33e50ba96b7231eb9ed3b36c2ee11ed945
sahana-n15 <sahananagaraja15@gmail.com>
2024-06-21 21:11:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const app = require('./app')
-
+// hello this is code code
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+// const app = require('./app')
+// // hello this is code code
+// const port = process.env.PORT || 3000
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
